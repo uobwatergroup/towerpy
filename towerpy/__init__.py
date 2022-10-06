f28a613585515764551ea0b371679bf966f3b806
enchilaDaSzen <eng.danielsr@outlook.com>
2022-10-06 12:22:50 +0200

    Initial commit

---
+++ towerpy/__init__.py
@@ -0,0 +1,31 @@
+"""
+Towerpy: an open-source toolbox for processing polarimetric radar data.
+
+@author: enchiladaszen
+"""
+
+# import subpackages
+from . import attc
+from . import calib
+from . import datavis
+from . import eclass
+from . import georad
+from . import io
+from . import ml
+from . import profs
+from . import qpe
+from . import utils
+
+_welcome_text = """
+You are using the Towerpy framework, an open source library for
+working with polarimetric weather radar data.
+
+If you find our work useful for your research, please consider citing our
+following publication:
+
+
+"""
+
+print(_welcome_text)
+
+__version__ = "v0.99-alpha"
---
+++ towerpy/base.py
@@ -0,0 +1,50 @@
+"""
+Towerpy: an open-source toolbox for processing polarimetric radar data.
+
+@author: enchiladaszen
+"""
+
+import traceback
+import logging
+
+
+class TowerpyError(Exception):
+
+    def __init__(self, message, inner=None):
+        super(TowerpyError, self).__init__()
+        self.message = 'TowerpyError: {}'.format(message)
+        self.inner = inner
+        self.traceback = traceback.format_stack()
+
+    def __str__(self):
+        return self.message
+
+
+class TowerpyLog(object):
+
+    def __init__(self, level=None):
+        self.logger = logging.getLogger()
+        if level:
+            handler = logging.StreamHandler()
+            self.logger.setLevel(level)
+        else:
+            handler = logging.NullHandler()
+        self.logger.addHandler(handler)
+
+    def debug(self, *args):
+        self.logger.debug(args[0].format(*args[1:]))
+
+    def info(self, *args):
+        self.logger.info(args[0].format(*args[1:]))
+
+    def warning(self, *args):
+        self.logger.warning(args[0].format(*args[1:]))
+
+    def error(self, *args):
+        self.logger.error(args[0].format(*args[1:]))
+
+    def exception(self, e):
+        self.logger.error('{} {}', e, traceback.format_exc())
+
+
+thelog = TowerpyLog()
